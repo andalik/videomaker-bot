fc5cb2375f7aa3db78c97fb6a5aba796842cc7c6
Renato Andalik <renato.andalik@ertech.com.br>
2019-06-15 18:01:03 -0300

    feat: start text robot

---
+++ robots/text.js
@@ -0,0 +1,63 @@
+// robo de texto
+// captura conteúdo da Wikipedia, limpa e quebra em sentenças
+const algorithmia = require('algorithmia')
+const sentenceBoundaryDetection = require('sbd')
+const algorithmiaApiKey = require('../credentials/algorithmia.json').apikey
+
+async function robot(content) {
+    await fetchContentFromWikipedia(content)
+    sanitizeContent(content)
+    breakContentIntoSentences(content)
+
+    async function fetchContentFromWikipedia(content) {
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+        const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+        const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaContent = wikipediaResponse.get()
+
+        content.sourceContentOriginal = wikipediaContent.content
+    }
+
+    function sanitizeContent(content) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+        const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+        
+        content.sourceContentSanitized = withoutDatesInParentheses
+
+        // remover linhas em branco
+        function removeBlankLinesAndMarkdown(text) {
+            const allLines = text.split('\n')
+            
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if(line.trim().length === 0 || line.trim().startsWith('=')) {
+                    return false
+                }
+
+                return true
+            })
+
+            return withoutBlankLinesAndMarkdown.join(' ')
+        }
+
+        // remover datas em parênteses
+        function removeDatesInParentheses(text) {
+            return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+        }
+    }
+
+    // quebrar o conteúdo em sentenças
+    function breakContentIntoSentences(content) {
+        content.sentences = []
+
+        const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+        sentences.forEach((sentences) => {
+            content.sentences.push({
+                text: sentences,
+                keywords: [],
+                images: []
+            })
+        })
+    }
+}
+
+module.exports = robot
