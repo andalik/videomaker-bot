113c7e20ab3ff03b52a458422d8062ecbba82623
Renato Andalik <renato.andalik@ertech.com.br>
2019-07-04 20:34:39 -0300

    feat: acrescentar robô de entrada e robô de estado

--- index.js
+++ index.js
@@ -1,49 +1,20 @@
 // Orquestrador
 
-const readline = require('readline-sync')
 const robots = {
-    text: require('./robots/text.js')
+    input: require('./robots/input.js'),
+    text: require('./robots/text.js'),
+    state: require('./robots/state.js')
 }
 
 async function start() {
-    // definição do objeto content (estrutura de dados)
-    const content = {
-        maximumSentences: 10
-    }
-
     console.log('Video Maker Bot')
     console.log(' by Andalik Industries')
 
-    content.language = askAndReturnLanguage()
-    content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
+    robots.input()
+    await robots.text()
 
-    await robots.text(content)
-
-    // askAndReturnLanguage
-    function askAndReturnLanguage() {
-        const languages = ['pt', 'en']
-        const selectLanguageIndex = readline.keyInSelect(languages, 'Selecione o idioma da pesquisa: ')
-        const selectLanguageText = languages[selectLanguageIndex]
-
-        return selectLanguageText
-    }
-
-    // askAndReturnSearchTerm
-    function askAndReturnSearchTerm() {
-        return readline.question('Digite o termo de busca na Wikipedia: ')
-    }
-
-    // askAndReturnPrefix
-    function askAndReturnPrefix() {
-        const prefixes = ['Quem é', 'Quem foi', 'O que é', 'O que foi', 'A história de', 'A história da', 'A história do']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Selecione uma opção: ')
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-
-        return selectedPrefixText
-    }
-
-    console.log(JSON.stringify(content, null, 4))
+    const content = robots.state.load()
+    console.dir(content, { depth: null })
 }
 
 start() 
