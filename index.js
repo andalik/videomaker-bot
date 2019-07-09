b62fca1e57f25cdf20b621777d47282d290839a1
Renato Andalik <renato.andalik@ertech.com.br>
2019-07-09 19:06:04 -0300

    feat: acrescentar robô de busca de imagens no Google

--- index.js
+++ index.js
@@ -3,15 +3,17 @@
 const robots = {
     input: require('./robots/input.js'),
     text: require('./robots/text.js'),
-    state: require('./robots/state.js')
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js')
 }
 
 async function start() {
     console.log('Video Maker Bot')
     console.log(' by Andalik Industries')
 
-    robots.input()
-    await robots.text()
+    //robots.input()
+    //await robots.text()
+    await robots.image()
 
     const content = robots.state.load()
     console.dir(content, { depth: null })
